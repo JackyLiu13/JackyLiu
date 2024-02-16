4c2c5266ea1a7b8f884754113c38d20abf6fc65a
Jacky <jackyliu1015@gmail.com>
2024-02-16 10:10:02 -0500

    project + gallery updates

--- js/date.js
+++ js/date.js
@@ -1,16 +1,13 @@
+var deadline = new Date("May 1, 2026 13:25:25").getTime();
 
-
-var deadline = new Date("May 1, 2025 13:25:25").getTime();
-
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   var now = new Date().getTime();
   var t = deadline - now;
-  var days = Math.floor(t / ((1000 * 60 * 60 * 24)));
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   var years = Math.floor(days / 365);
-  days = Math.floor(days % 365)
-  var months = Math.floor(days / 30.4167) % 12
-  days = Math.floor(days % 30.4167)
+  days = Math.floor(days % 365);
+  var months = Math.floor(days / 30.4167) % 12;
+  days = Math.floor(days % 30.4167);
   var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((t % (1000 * 60)) / 1000);
@@ -23,11 +20,11 @@ var x = setInterval(function() {
   if (t < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "Graduated 🎓";
-    document.getElementById("year").innerHTML = '0';
-    document.getElementById("month").innerHTML = '0';
-    document.getElementById("day").innerHTML = '0';
-    document.getElementById("hour").innerHTML = '0';
-    document.getElementById("minute").innerHTML = '0';
-    document.getElementById("second").innerHTML = '0';
+    document.getElementById("year").innerHTML = "0";
+    document.getElementById("month").innerHTML = "0";
+    document.getElementById("day").innerHTML = "0";
+    document.getElementById("hour").innerHTML = "0";
+    document.getElementById("minute").innerHTML = "0";
+    document.getElementById("second").innerHTML = "0";
   }
 }, 1000);
